738204320b70a1d9d66df325a76380985d8512ca
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2021-01-29 09:16:20 +0100

    arg parse

--- jenky/__main__.py
+++ jenky/__main__.py
@@ -1,10 +1,12 @@
 import json
+import sys
 import time
 from pathlib import Path
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
 from jenky import util
 
@@ -13,8 +15,8 @@ app.mount("/static", StaticFiles(directory="jenky/html"), name="mymountname")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_root():
+    return RedirectResponse(url='/static/index.html')
 
 
 @app.get("/repos")
@@ -64,8 +66,19 @@ def post_repo(repo_id: str, action: GitAction):
 
     return dict(repo_id=repo_id, action=action.action, message=message)
 
+config_file = 'config.json'
+port = 8000
 
-config = json.loads(Path('config.json').read_text(encoding='utf8'))
+# TODO: Use argparse
+for arg in sys.argv:
+    if arg.startswith('--config='):
+        config_file = arg.split('=')[1]
+    elif arg.startswith('--port='):
+        port = int(arg.split('=')[1])
+
+config_file = Path(config_file)
+print(f'Reading config from {config_file}')
+config = json.loads(config_file.read_text(encoding='utf8'))
 
 
-uvicorn.run(app, host="localhost", port=8000)
+uvicorn.run(app, host="localhost", port=port)
