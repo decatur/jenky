ad5b63eb0e11db2b21300fe7b2f095554b2562b2
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2021-02-21 20:11:22 +0100

    add jenky to config

--- run_jenky.sh
+++ run_jenky.sh
@@ -1,2 +1,3 @@
 # Run jenky server in background.
-nohup venv/bin/python -m jenky --app_config=jenky_app_config.json --port=8094&
+nohup venv/bin/python -m jenky --app_config=jenky_app_config.json --port=8094 cmd >jenky.out 2>&1 &
+echo $! > jenky.pid
