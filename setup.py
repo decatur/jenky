95006fb2559df06e8969b4d9c826b3b82e97f7bb
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2021-02-22 20:48:32 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_data = {'': ['*'], 'jenky': ['html/*']}
 
 dist = setup(
     name='jenky',
-    version='0.0.3',
+    version='0.0.4',
     author="Wolfgang Kühn",
     description="A build and deploy server for Python developers",
     packages=['jenky'],
