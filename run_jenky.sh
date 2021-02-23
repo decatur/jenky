2ae0c935b5679212af9584258b63ed7c4c730194
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2021-02-23 22:18:11 +0100

    process contract

--- run_jenky.sh
+++ run_jenky.sh
@@ -1,4 +1,19 @@
-# Run jenky server in background.
+# Runs jenky server in background.
+# This is a template for the contract jenky needs for running processes:
+# 1) A process is identified by both
+# 1.1) A pid-file containing the PID of the process, and
+# 1.2) The env var JENKY_NAME.
+# 2.) Both stdout and stderr are directed to the file of said name
+#
+# 1.1 is for performance reasons but cannot be used to identify the process as PIDs are reused.
+# 1.2 is used to verify that the process to which the pid-file points to is genuine.
+
+if strings /proc/$(cat jenky.pid)/environ | egrep JENKY_NAME=jenky
+then
+  kill $(cat jenky.pid)
+fi
+
+cd jenky_dir
 export JENKY_NAME=jenky
-nohup venv/bin/python -m jenky --app_config=jenky_app_config.json --port=8094 >jenky.out 2>&1 &
-echo $! > jenky.pid
+nohup venv/bin/python -m jenky --app_config=jenky_app_config.json --port=8094 >$JENKY_NAME.out 2>&1 &
+echo $! > $JENKY_NAME.pid
